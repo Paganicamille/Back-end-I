2899af7268d880e308355b5e00c7523d7b648007
Paganicamille <camille.dias@estudante.ifro.edu.br>
2025-04-03 16:40:59 -0400

    first commit

---
+++ aula01.js
@@ -0,0 +1,10 @@
+let valor = 13.141516;
+let texto = 'Meu primeiro projeto em JavaScript';
+let bool = true;
+
+console.log(valor.toFixed(2)); //arredonda o valor para duas casas decimais;
+console.log(typeof valor); // retorna o tipo da variavel;
+console.log(texto.toUpperCase()); // converte para maiusculos;
+console.log(typeof texto()); // retorna o tipo da variavel;
+console.log(ativo.toString()); //retorna o tipo da variavel;
+console.log(typeof ativo());// retorna o tipo da variavel;
